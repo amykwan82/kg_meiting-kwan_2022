64aed6bc47b5ea6852fc997ad36e9e5341e96e8d
Mei Ting kWAN <meitingkw82@gmail.com>
2021-05-03 14:55:55 -0400

    Cleaned up files

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ var current_str = ""
 var solution = ""
 var numList = ["Zero","One","Two","Three","Four","Five","Six","Seven","Eight","Nine"]
 
+//from the first integer given from the arguments
 for (var i = 2; i< process.argv.length;i ++)  {
   var current_num = process.argv[i]
   
@@ -11,11 +12,13 @@ for (var i = 2; i< process.argv.length;i ++)  {
     current_num = Math.floor(current_num/10);
   }
   
-  solution += current_str 
+  //adding to the output
+  solution += current_str
   if (i != process.argv.length - 1) {
     solution += ","
   }
   
+  //resetting for next number
   current_str = ""
 }
 
